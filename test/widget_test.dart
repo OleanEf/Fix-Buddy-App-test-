30c1ee13663dba29e3fa8512f919e2abda85163f
Olean <asadpepop@gmail.com>
2025-01-14 02:09:48 +0700

    commit 2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:project_app/main.dart';
+import 'package:project_app/register.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
